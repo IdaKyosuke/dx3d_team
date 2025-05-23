b1d18529999cba86376255039da62d7291468cd6
hmj2422030 <hmj2422030@stu.o-hara.ac.jp>
2025-05-23 14:47:00 +0900

    時間停止中のフィルターを用意。

--- ScreenFilter.h
+++ ScreenFilter.h
@@ -1,19 +1,29 @@
 #pragma once
 #include"Actor.h"
+#include"Transform.h"
 #include<vector>
 
 class LoadPlayer;
 
-class ScreenFilter : public Actor
+class ScreenFilter : public Node
 {
 private:
+	static constexpr int FilterNum = 3;
 	static constexpr int MaxPal = 150;	// 輝度の最大値
 	// 画面のフィルター用
-	static const char* FilterTexture[2];
+	static const char* FilterTexture[FilterNum];
 	LoadPlayer* m_player;
+	Sprite m_redImg;
+	Sprite m_grayImg;
+	Sprite m_whiteImg;
+
+	Transform m_transform;
 
 protected:
-	void Draw() override;
+	virtual void Draw() override;
+	virtual void Release() override;
+	virtual void Update() override;
+	virtual void Load() override;
 
 public:
 	ScreenFilter(LoadPlayer* player);
